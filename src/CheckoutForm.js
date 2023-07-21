0566876df4049ed27eb981ffa579d07a7b185b96
shivam <shivam.shashank@juspay.in>
2023-07-21 18:19:50 +0530

    modified:   src/CheckoutForm.js

--- src/CheckoutForm.js
+++ src/CheckoutForm.js
@@ -54,7 +54,7 @@ export default function CheckoutForm() {
         </span>
       </button>
       {/* Show any error or success messages */}
-      {message && <div id="payment-message">{message}</div>}
+      {message && <div id="payment-message">{JSON.stringify(message)}</div>}
     </form>
   );
 }
